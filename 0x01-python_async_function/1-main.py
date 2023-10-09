7acc14ae1ba9a3a15499c858c2a2bc1b7f0885d6
mainangaruiya <003paulmaina@gmail.com>
2023-10-09 19:09:30 +0300

    main

---
+++ 0x01-python_async_function/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Test file 
+'''
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+print(asyncio.run(wait_n(5, 5)))
+print(asyncio.run(wait_n(10, 7)))
+print(asyncio.run(wait_n(10, 0)))
+
