fbcd05b37fabd8cce55ee6acd7fbb608837fa212
mainangaruiya <003paulmaina@gmail.com>
2023-10-09 18:13:34 +0300

    mport wait_random from the previous python file that you’ve written and write an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn wait_random n times with the specified max_delay.
    wait_n should return the list of all the delays (float values). The list of the delays should be in ascending order without using sort() because of concurrency.

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+task 1 module
+'''
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
+    '''
+    wait_times = await asyncio.gather(
+        *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
